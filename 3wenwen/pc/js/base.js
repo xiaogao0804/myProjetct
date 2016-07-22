d434115b4ac89eca573bc283b83c2e4e81649f32
xiaogao0804 <1941828283@qq.com>
2016-07-22 15:25:53 +0800

    3wenw

---
+++ 3wenwen/pc/js/base.js
@@ -0,0 +1,26 @@
+(function(){
+	var i=0
+	var x=["head","head_1","head_2"]
+	setInterval(function(){
+		i++;
+		if(i>2)
+		{
+			i=0;
+		}else{
+			$('.headBg').fadeOut(500,function(){
+				$('.headBg').fadeIn(500);
+				$('.headBg').find("img").attr("src","img/"+x[i]+".png");
+				
+			});
+		}
+	},4500);
+})();
+//大图切换end
+$('.mleftNav').on('click',function(){
+	$('.topNav').slideToggle();
+});
+$('.dynamicLeft').height(parseInt($('.dynamicLeft').width()));
+$('.toTop').on('click',function(){
+	$('body').animate({'scrollTop':0});
+})
+
